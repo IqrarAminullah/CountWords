f5a840d59d08c7da467f03ed82153ed0c71883ba
IqrarAminullah <Iqrar.silveria@gmail.com>
2024-02-07 04:27:54 +0700

    Configuring Main Code with Parameters

--- CSG/src/main/java/CountWords/Main.java
+++ CSG/src/main/java/CountWords/Main.java
@@ -10,13 +10,22 @@ public class Main {
      * @param args filename
      */
     public static void main(String[] args){
+        //argument empty checking
+        if (args.length == 0){
+            logger.log(Level.SEVERE,"No arguments were profivded");
+            System.exit(0);
+        }
         try{
             Words w = new Words(args[0]);
+            char c = args[1] == null ? 'm' : args[1].charAt(0); //reference char for words to count
+            int l = args[2] == null? 5 : Integer.parseInt(args[2]); //reference length for words to count
+
             System.out.println(
-                    "Number of Words beginning with Letter \"M\" or \"M\" : "
-                            + w.countWordsBeginWith('m'));
-            System.out.println("Number of Words with Length 5 or more : "
-                    + w.countWordsWithLength(5));
+                    "Number of Words beginning with Letter " + Character.toUpperCase(c) +
+                    " or " + Character.toLowerCase(c) + " : " +
+                            + w.countWordsBeginWith(c));
+            System.out.println("Number of Words with Length "+ l + " or more : "
+                    + w.countWordsWithLength(l));
         } catch (Exception e) {
             logger.log(Level.SEVERE, "Error Main Function" + "\n" + e);
         }
